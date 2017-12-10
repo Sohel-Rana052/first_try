a63a6323059d61ca70c0dae75e515566e9d1e169
SohelRanaBSMRSTU <sohelranacse052@gmail.com>
2017-12-10 10:44:57 +0600

    first commit

---
+++ program111.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(!(n==29))
+    {
+        cout<<n;
+    cin>>n;}
+    cout<<".\n..\n..\nEnd of program\n";
+
+    return 0;//(cout<<"End of the program";);
+}
